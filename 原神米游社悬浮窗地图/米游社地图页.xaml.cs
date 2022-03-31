862e2bf7059f431bda1afbc17e1dfac3918e1e6b
Gode King <GengGode@outlook.com>
2022-03-31 22:42:04 +0800

    添加项目文件。

---
+++ 原神米游社悬浮窗地图/米游社地图页.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace 原神米游社悬浮窗地图
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class 米游社地图页 : Page
+    {
+        public 米游社地图页()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
